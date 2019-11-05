72fe2705d79d6e276fbb198709adf3226a4140a7
LWJ <rhkdqhrehddl@naver.com>
2019-11-05 13:38:30 +0900

    이력서 작성 UI 일부 구현, 컨트롤러 수정 [2019/11/05]

--- SPRING/ParkHyeonChan/4sCoverMain/src/main/java/com/coverletter/main/controller/ServiceController.java
+++ SPRING/ParkHyeonChan/4sCoverMain/src/main/java/com/coverletter/main/controller/ServiceController.java
@@ -1,5 +1,9 @@
 package com.coverletter.main.controller;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
+import com.coverletter.main.data.ResumeVO;
 import com.coverletter.main.parameter.RegisterParam;
 
 @CrossOrigin
@@ -25,5 +30,19 @@ public class ServiceController {
 		
 		//7.index로 이동
 		return new ModelAndView(new RedirectView("/Form/index", true));
+	}
+	
+	@RequestMapping("/select_resume")
+	//추후에 userID를 매개변수로 받아 User에 해당하는 이력서 목록만 조회
+	public List<ResumeVO> selectResume() {
+		List<ResumeVO> resume_list = new ArrayList<ResumeVO>();
+		
+		resume_list.add(new ResumeVO(1, "회사와 사회에 모두 기여할 수 있는 인재 입니다", new Date()));
+		resume_list.add(new ResumeVO(2, "회사와 사회에 모두 기여할 수 있는 인재 입니다", new Date()));
+		resume_list.add(new ResumeVO(3, "회사와 사회에 모두 기여할 수 있는 인재 입니다", new Date()));
+		resume_list.add(new ResumeVO(4, "회사와 사회에 모두 기여할 수 있는 인재 입니다", new Date()));
+		resume_list.add(new ResumeVO(5, "회사와 사회에 모두 기여할 수 있는 인재 입니다", new Date()));
+		
+		return resume_list;
 	}
 }
